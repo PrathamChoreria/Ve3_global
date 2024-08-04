01e95fe770c59b336278a703b0122ff1f293b953
PRATS CHORERIA <prathamchoreria@gmail.com>
2024-08-04 23:57:19 +0530

    Added changes

--- analysis_pro/analysis/views.py
+++ analysis_pro/analysis/views.py
@@ -45,56 +45,6 @@ def index(request):
 
 
 
-def delete_file(request):
-    if request.method == 'POST':
-        file_name = request.POST.get('file_name')
-        if file_name:
-            file_path = os.path.join(settings.MEDIA_ROOT, file_name)
-            if os.path.exists(file_path):
-                os.remove(file_path)
-        return HttpResponseRedirect(reverse('index')) 
-
-
-
-
-def handle_outliers(df, numerical_cols):
-    for col in numerical_cols:
-        # Calculate the first (Q1) and third (Q3) quartiles
-        Q1 = df[col].quantile(0.25)
-        Q3 = df[col].quantile(0.75)
-        IQR = Q3 - Q1
-        
-        # Define the outlier bounds
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        
-        # Clip outliers
-        df[col] = np.clip(df[col], lower_bound, upper_bound)
-    
-    return df
-
-
-
-
-def count_outliers(df, numerical_cols):
-    outlier_counts = {}
-    for col in numerical_cols:
-        Q1 = df[col].quantile(0.25)
-        Q3 = df[col].quantile(0.75)
-        IQR = Q3 - Q1
-        
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        
-        # Count outliers
-        outliers = df[(df[col] < lower_bound) | (df[col] > upper_bound)]
-        outlier_counts[col] = len(outliers)
-    
-    return outlier_counts
-
-
-
-
 def data_preprocessing(file_path, f):
     df = pd.read_csv(file_path)
     
@@ -110,7 +60,6 @@ def data_preprocessing(file_path, f):
         columns_to_drop = missing_percentage[missing_percentage > 30].index
         df.drop(columns=columns_to_drop, inplace=True)
         
-        # Process each column with missing values
         for column in df.columns:
             if df[column].isnull().sum() > 0:
                 if df[column].dtype in ['int64', 'float64']:
@@ -126,13 +75,15 @@ def data_preprocessing(file_path, f):
                     # Fill categorical columns with the most frequent value (mode)
                     df[column].fillna(df[column].mode()[0], inplace=True)
                     
-    # Removing duplicates if there are any in the CSV file, except for 'Date' column
-    df.drop_duplicates(subset=[col for col in df.columns if col != 'Date'], inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    # Identify numerical columns for scaling
     numerical_cols = df.select_dtypes(include=['int64', 'float64']).columns
+
+    preprocessed_file_path = os.path.join(settings.MEDIA_ROOT, 'preprocessed1', f.name)
+    os.makedirs(os.path.dirname(preprocessed_file_path), exist_ok=True)
+    df.to_csv(preprocessed_file_path, index=False)
     
-    # Scale numerical features
+    # Scale numerical features for machine learning models if needed
     scaler = MinMaxScaler()
     df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
     
@@ -149,6 +100,90 @@ def data_preprocessing(file_path, f):
     return preprocessed_file_path
 
 
+
+
+def visuals(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_path,preprocessed_file = handle_uploaded_file(request.FILES['file'])
+            df = pd.read_csv(file_path)
+            df1 = pd.read_csv(preprocessed_file)
+
+            numerical_columns = df.select_dtypes(include=['number','int64', 'float64']).columns
+            numerical_columns1=df1.select_dtypes(include=['number','int64', 'float64']).columns
+            categorical_columns = df.select_dtypes(include='object').columns
+            datetime_columns = df.select_dtypes(include=['datetime']).columns
+            categorical_columns = categorical_columns.difference(datetime_columns)
+            categorical_columns1 = df1.select_dtypes(include='object').columns
+
+
+            head = df.head().to_html()
+            describe_data = df.describe().to_html()
+            missing_values = df.isnull().sum().to_frame(name='Missing Values').to_html()
+            duplicate_values =  df.duplicated().sum()
+            outlier_counts = count_outliers(df, numerical_columns)
+            outlier_counts_html = pd.DataFrame(outlier_counts.items(), columns=['Column', 'Number of Outliers']).to_html(index=False)
+
+            
+
+            mean_values = df[numerical_columns].mean().to_frame(name='Mean').to_html()
+            median_values = df[numerical_columns].median().to_frame(name='Median').to_html()
+            mode_values = df[categorical_columns].mode().iloc[0].to_frame(name='Mode').to_html()
+            std_values = df[numerical_columns].std().to_frame(name='Standard Deviation').to_html()
+
+            hist_paths = []
+            hist_choices = []
+            plot_paths = []
+            plot_choices = []
+            pie_paths = []
+            pie_choices = []
+            scaled_paths=[]
+            scaled_choices=[]
+
+
+            scatter_paths, scatter_choices = create_scatter_plots(df, numerical_columns,'others')
+            scaled_scatter_paths,scaled_scatter_choices = create_scatter_plots(df1,numerical_columns1,'scaled')
+            scaled_line_paths,scaled_line_choices=create_line_plots(df1,numerical_columns1,'scaled')
+            line_paths, line_choices = create_line_plots(df, numerical_columns,'others')
+            pie_paths, pie_choices = create_pie_charts(df, categorical_columns,'pie')
+            histogram_paths, histogram_choices = create_histograms(df, numerical_columns,'histograms')
+            scaled_histograms_path,scaled_histograms_choices=create_histograms(df1,numerical_columns1,'scaled')
+
+            hist_paths.extend(histogram_paths)
+            hist_choices.extend( histogram_choices)
+            plot_paths.extend(scatter_paths + line_paths )
+            plot_choices.extend(scatter_choices + line_choices)
+            pie_paths.extend(pie_paths)
+            pie_choices.extend(pie_choices)
+            scaled_paths.extend(scaled_histograms_path+scaled_line_paths+scaled_scatter_paths)
+            scaled_choices.extend(scaled_histograms_choices+scaled_line_choices+scaled_scatter_choices)
+
+            # print(head)
+            # print(describe_data)
+
+            return render(request, 'analysis/results.html', {
+                'head': head,
+                'describe': describe_data,
+                'missing_values': missing_values,
+                'duplicated_values':duplicate_values,
+                'mean_values': mean_values,
+                'median_values': median_values,
+                'mode_values': mode_values,
+                'std_values': std_values,
+                'hist_paths': hist_paths,
+                'hist_choices': hist_choices,
+                'plot_paths':plot_paths,
+                'plot_choices': plot_choices,
+                'pie_paths': pie_paths,
+                'pie_choices':pie_choices,
+                'scaled_paths':scaled_paths,
+                'scaled_choices':scaled_choices,
+                'outlier_counts': outlier_counts_html,
+            })
+    else:
+        form = UploadFileForm()
+    return render(request, 'analysis/index.html', {'form': form})
 
 def create_scatter_plots(df, numerical_columns,name):
     plot_paths = []
@@ -238,84 +273,51 @@ def create_histograms(df, numerical_columns,name):
 
 
 
-
-def visuals(request):
+def delete_file(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            file_path,preprocessed_file = handle_uploaded_file(request.FILES['file'])
-            df = pd.read_csv(file_path)
-            df1 = pd.read_csv(preprocessed_file)
-
-            numerical_columns = df.select_dtypes(include=['number','int64', 'float64']).columns
-            numerical_columns1=df1.select_dtypes(include=['number','int64', 'float64']).columns
-            categorical_columns = df.select_dtypes(include='object').columns
-            datetime_columns = df.select_dtypes(include=['datetime']).columns
-            categorical_columns = categorical_columns.difference(datetime_columns)
-            categorical_columns1 = df1.select_dtypes(include='object').columns
+        file_name = request.POST.get('file_name')
+        if file_name:
+            file_path = os.path.join(settings.MEDIA_ROOT, file_name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        return HttpResponseRedirect(reverse('index')) 
 
 
-            head = df.head().to_html()
-            describe_data = df.describe().to_html()
-            missing_values = df.isnull().sum().to_frame(name='Missing Values').to_html()
-            duplicate_values =  df.duplicated().sum()
-            outlier_counts = count_outliers(df, numerical_columns)
-            outlier_counts_html = pd.DataFrame(outlier_counts.items(), columns=['Column', 'Number of Outliers']).to_html(index=False)
-
-            
-
-            mean_values = df[numerical_columns].mean().to_frame(name='Mean').to_html()
-            median_values = df[numerical_columns].median().to_frame(name='Median').to_html()
-            mode_values = df[categorical_columns].mode().iloc[0].to_frame(name='Mode').to_html()
-
-            hist_paths = []
-            hist_choices = []
-            plot_paths = []
-            plot_choices = []
-            pie_paths = []
-            pie_choices = []
-            scaled_paths=[]
-            scaled_choices=[]
 
 
-            scatter_paths, scatter_choices = create_scatter_plots(df, numerical_columns,'others')
-            scaled_scatter_paths,scaled_scatter_choices = create_scatter_plots(df1,numerical_columns1,'scaled')
-            scaled_line_paths,scaled_line_choices=create_line_plots(df1,numerical_columns1,'scaled')
-            line_paths, line_choices = create_line_plots(df, numerical_columns,'others')
-            pie_paths, pie_choices = create_pie_charts(df, categorical_columns,'pie')
-            histogram_paths, histogram_choices = create_histograms(df, numerical_columns,'histograms')
-            scaled_histograms_path,scaled_histograms_choices=create_histograms(df1,numerical_columns1,'scaled')
+def handle_outliers(df, numerical_cols):
+    for col in numerical_cols:
+        # Calculate the first (Q1) and third (Q3) quartiles
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        
+        # Define the outlier bounds
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        
+        # Clip outliers
+        df[col] = np.clip(df[col], lower_bound, upper_bound)
+    
+    return df
 
-            hist_paths.extend(histogram_paths)
-            hist_choices.extend( histogram_choices)
-            plot_paths.extend(scatter_paths + line_paths )
-            plot_choices.extend(scatter_choices + line_choices)
-            pie_paths.extend(pie_paths)
-            pie_choices.extend(pie_choices)
-            scaled_paths.extend(scaled_histograms_path+scaled_line_paths+scaled_scatter_paths)
-            scaled_choices.extend(scaled_histograms_choices+scaled_line_choices+scaled_scatter_choices)
 
-            # print(head)
-            # print(describe_data)
 
-            return render(request, 'analysis/results.html', {
-                'head': head,
-                'describe': describe_data,
-                'missing_values': missing_values,
-                'duplicated_values':duplicate_values,
-                'mean_values': mean_values,
-                'median_values': median_values,
-                'mode_values': mode_values,
-                'hist_paths': hist_paths,
-                'hist_choices': hist_choices,
-                'plot_paths':plot_paths,
-                'plot_choices': plot_choices,
-                'pie_paths': pie_paths,
-                'pie_choices':pie_choices,
-                'scaled_paths':scaled_paths,
-                'scaled_choices':scaled_choices,
-                'outlier_counts': outlier_counts_html,
-            })
-    else:
-        form = UploadFileForm()
-    return render(request, 'analysis/index.html', {'form': form})
+
+def count_outliers(df, numerical_cols):
+    outlier_counts = {}
+    for col in numerical_cols:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        
+        # Count outliers
+        outliers = df[(df[col] < lower_bound) | (df[col] > upper_bound)]
+        outlier_counts[col] = len(outliers)
+    
+    return outlier_counts
+
+
